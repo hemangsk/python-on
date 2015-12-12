748649f86f83aa8e2129c753806ffbb26d38aae1
Hemang Kumar <hemangsk@gmail.com>
2015-12-12 19:04:46 +0530

    Not much

---
+++ testingscript.py
@@ -0,0 +1,13 @@
+import wget
+import urllib2
+from BeautifulSoup import BeautifulSoup
+Soup=BeautifulSoup
+import BeautifulSoup
+i=0
+url="http://stim.ee.uh.edu/education/ece-3340-numerical-methods/"
+f=urllib2.urlopen(url)
+page=f.read()
+f.close()
+soup=Soup(page)
+for link in soup.findAll('a', href=True):
+    print link['href']
--- x.py
+++ x.py
@@ -4,7 +4,7 @@ import wget
 import requests
 Soup=BeautifulSoup
 import BeautifulSoup
-url="http://xampaperz.com/xampaperz/xampaperz/MAIT/xampaperz/subject.php?id=1&semkey=8&sem=8thsemcse&semester=8th&main=xampaerl_cse&stream_get=Computer"+ "%20"+"Science"
+url="http://xampaperz.com/xampaperz/xampaperz/MAIT/xampaperz/subject.php?id=1&semkey=7&sem=7thsemcse&semester=7th&main=xampaerl_cse&stream_get=Computer"+ "%20"+"Science"
 f=urllib2.urlopen(url)
 page=f.read()
 f.close()
@@ -35,15 +35,17 @@ for a in fresh:
             paper.append("http:xampaperz/xampaperz/MAIT/xampaperz/" + a['href'])
 final=[]
 for a in paper:
-    a= a[a.find("&paper_url")+len("&paper_url")+1:a.find(".jpg")] +".jpg"
+    a= a[a.find("&paper_url")+len("&paper_url")+1:a.find(".jpg")] +"2.jpg"
+    b= a[a.find("&paper_url")+len("&paper_url")+1:a.find(".jpg")] +".jpg"
     print a
     final.append(a)
+    final.append(b)
 
 i=0
 for x in final:
     response = requests.get(x)
     if response.status_code == 200:
-        f = open("8th Sem final" + str(i)+ ".jpg", 'wb')
+        f = open("7th Sem  Part 2 final" + str(i)+ ".jpg", 'wb')
         f.write(response.content)
         f.close()
     i=i+1
